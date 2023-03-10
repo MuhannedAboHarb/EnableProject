e4b507fd2fa09104d77ef3d74b0193035dac6206
MuhannedAboHarb <125630547+MuhannedAboHarb@users.noreply.github.com>
2023-03-10 23:19:50 +0200

    First commit

---
+++ resources/views/cms/temp.blade.php
@@ -0,0 +1,16 @@
+@extends('cms.parent')
+
+@section('title','');
+@section('page-big-title','')
+@section('page-main-title','')
+@section('page-sub-title','')
+
+@section('styles')
+@endsection
+
+@section('content')
+@endsection
+
+
+@section('scripts')
+@endsection
